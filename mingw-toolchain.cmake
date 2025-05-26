3f952889339757cfa4cc18172fdcc620e2f13869
Lokej23 <damian.gol2004@gmail.com>
2025-05-26 21:24:51 +0200

    Dodanie kompilacji dla systemu Windows

---
+++ mingw-toolchain.cmake
@@ -0,0 +1,15 @@
+# mingw-toolchain.cmake
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+
+# Ścieżka do wypakowanej biblioteki SDL2 dla Windows
+set(SDL2_ROOT "${CMAKE_SOURCE_DIR}/deps/SDL2-2.32.6/x86_64-w64-mingw32")
+
+# Poprawne wyszukiwanie include/lib z SDL2
+set(CMAKE_FIND_ROOT_PATH ${SDL2_ROOT})
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
